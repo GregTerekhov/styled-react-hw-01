ec4bb72fd15a11d8447c1da904209fc0bb9ed721
GregTerekhov <113180004+GregTerekhov@users.noreply.github.com>
2023-04-10 05:57:57 +0300

    Added global styles

--- src/index.js
+++ src/index.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from 'components/App';
-import './index.css';
+import { Global, css } from '@emotion/react';
+import { App } from 'components/App/App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <Global
+      styles={css`
+        body {
+          margin: 0;
+          font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto',
+            'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans',
+            'Helvetica Neue', sans-serif;
+          -webkit-font-smoothing: antialiased;
+          -moz-osx-font-smoothing: grayscale;
+        }
+        code {
+          font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
+            monospace;
+        }
+      `}
+    />
     <App />
   </React.StrictMode>
 );
